b928ff630eceaee283cf0ba04dcdc6a855c4d1ab
saram <sara.mashael@student.ehb.be>
2020-04-26 16:40:53 +0200

    hallo

--- src/hallo/Iets.java
+++ src/hallo/Iets.java
@@ -2,6 +2,6 @@ package hallo;
 
 public class Iets {
     public static void main(String[] args) {
-        System.out.println("nog een keer Hallo world!!");
+        System.out.println("nog een keer Hallo world!");
     }
 }
